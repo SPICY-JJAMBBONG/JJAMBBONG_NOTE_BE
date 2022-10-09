d4f116b0e2e85dd148829a8b76f7258d164204bb
minnczi <kmj1995kr@gmail.com>
2022-10-09 16:39:52 +0900

    ADD - Block Image 관련 Entity, DTO, Mapper 추가

--- src/main/java/com/jjambbong/note/mapper/BlockMapper.java
+++ src/main/java/com/jjambbong/note/mapper/BlockMapper.java
@@ -1,6 +1,7 @@
 package com.jjambbong.note.mapper;
 
 import com.jjambbong.note.dto.BlockDto;
+import com.jjambbong.note.entity.BlockImage;
 import com.jjambbong.note.entity.BlockPage;
 import com.jjambbong.note.entity.BlockText;
 import org.modelmapper.ModelMapper;
@@ -13,9 +14,8 @@ public class BlockMapper {
     @Autowired
     ModelMapper modelMapper;
 
+    // DTO -> ENTITY
     public BlockPage BlockDtoToBlockPage (BlockDto block) {
-        Class<BlockPage> blockPageClass = BlockPage.class;
-        BlockPage blockPage = modelMapper.map(block, BlockPage.class);
         return modelMapper.map(block, BlockPage.class);
     }
 
@@ -24,12 +24,22 @@ public class BlockMapper {
 
     }
 
+    public BlockImage BlockDtoToBlockImage (BlockDto block) {
+        return modelMapper.map(block, BlockImage.class);
+    }
+
+
+    // ENTITY -> DTO
     public BlockDto BlockPageToBlockDto (BlockPage blockPage) {
         return modelMapper.map(blockPage, BlockDto.class);
     }
 
     public BlockDto BlockTextToBlockDto (BlockText blockText) {
         return modelMapper.map(blockText, BlockDto.class);
+    }
+
+    public BlockDto BlockImageToBlockDto (BlockImage blockImage) {
+        return modelMapper.map(blockImage, BlockDto.class);
     }
 
 }
